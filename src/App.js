818a761f6693d271463a3065b6cde4a441812832
sascharissling <sascha.rissling@gmail.com>
2019-12-03 11:47:12 +0100

    finish app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "@emotion/styled";
+import GlobalStyles from "./GlobalStyles";
+
+//STYLE Start
+
+const AppContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+`;
+
+//STYLE End
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <AppContainer>Memes</AppContainer>;
 }
 
 export default App;
