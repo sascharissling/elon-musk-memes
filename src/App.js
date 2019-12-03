e47ebda2514c02af1e0ce2b6e6c38fe6ca39f198
Sascha Rissling <53901545+sascharissling@users.noreply.github.com>
2019-12-03 12:27:12 +0100

    add design to buttons and background as well as hover effects (#2)

--- src/App.js
+++ src/App.js
@@ -9,12 +9,57 @@ const AppContainer = styled.div`
   display: flex;
   flex-flow: column nowrap;
   align-items: center;
+  justify-content: center;
+`;
+
+const SubmitButton = styled.button`
+  width: 55%;
+  height: 15%;
+  font-family: monospace;
+  font-size: 40px;
+  background: transparent;
+  border-radius: 15px;
+  background: linear-gradient(to right, #544a7d, #ffd452);
+  box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+  color: white;
+  &:hover {
+    color: whitesmoke;
+    background: linear-gradient(to top, #ff416c, #ff4b2b);
+    box-shadow: 10px 10px 17px 0px rgba(0, 0, 0, 0.75);
+  }
+`;
+
+const RandomButton = styled.button`
+  width: 55%;
+  height: 15%;
+  font-family: monospace;
+  background: transparent;
+  font-size: 40px;
+  border-radius: 15px;
+  background: linear-gradient(to top, #8a2387, #e94057, #f27121);
+  box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+  color: white;
+  &:hover {
+    color: whitesmoke;
+    background: linear-gradient(to right, #009fff, #ec2f4b);
+    box-shadow: 10px 10px 17px 0px rgba(0, 0, 0, 0.75);
+  }
 `;
 
 //STYLE End
 
 function App() {
-  return <AppContainer>Memes</AppContainer>;
+  return (
+    <>
+      <GlobalStyles />
+      <AppContainer>
+        <SubmitButton>SUBMIT</SubmitButton>Elon Meme
+        <RandomButton>RANDOM</RandomButton>
+      </AppContainer>
+    </>
+  );
 }
-
 export default App;
+
+// On Random Button click => Background changes & Meme changes
+// On Submit Button click => Meme URL submit + background color
